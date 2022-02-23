39407907974c8a660fc48436958d89c7d07d9885
Mehrdad <amini_pk@hotmail.com>
2022-02-23 16:10:18 +0100

    Add styles

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Instructions
+
+## How black jack works ?
+* get a hand that sum get 21 and not above 21
+* Facecards (King, queen, soldier) = 10 and Ace = 11
+* if sum is less than or equel to 21 => win
+* if sum is above 21 => lose
--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-/* Instructions*/
-
-/* How black jack works ? */
-// get a hand that sum get 21 and not above 21
-
-// Facecards (King, queen, soldier) = 10
-// Ace = 11
-
-// if sum is less than or equel to 21 => win
-// if sum is above 21 => lose
-
-
 let firstCard = 10;
 let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-// 1. Declare a variable called message and assign its value to an empty string
 let message = "";
-// 2. Reassign the message variable to the string we're logging out
+
 if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂";
 } else if (sum === 21) {
@@ -28,5 +15,4 @@ if (sum <= 20) {
     isAlive = false;
 }
 
-// 3. Log it out!
 console.log(message);
