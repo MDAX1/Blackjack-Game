589f06203a027e5ce9728bdf3613308071584946
Mehrdad <amini_pk@hotmail.com>
2022-02-24 08:08:25 +0100

    Display the sum

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
-
-// 1. Store the message-el paragraph in a variable called messageEl
+// 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el");
 let messageEl = document.getElementById("message-el");
-console.log(messageEl);
+// let sumEl = document.querySelector("#sum-el");
 
 function startGame() {
+    // 3. Render the sum on the page using this format -> "Sum: 14"
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
-        message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Blackjack!"
-        hasBlackJack = true
+        message = "Blackjack!";
+        hasBlackJack = true;
     } else {
-        message = "You're out of the game!"
-        isAlive = false
+        message = "You're out of the game!";
+        isAlive = false;
     }
-    // 2. Display the message in the messageEl using messageEl.textContent
     messageEl.textContent = message;
 }
